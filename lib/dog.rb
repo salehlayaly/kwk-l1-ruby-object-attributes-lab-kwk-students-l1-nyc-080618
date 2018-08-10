ac494a81e455117b104f0fc19904c543ab51e6c8
salehlayaly <lsaleh@hcstonline.org>
2018-08-10 15:39:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,38 @@
 # dog.rb
+class Dog 
+  
+  def name
+    @name 
+  end 
+  
+  def name= (name) 
+    @name = name 
+  end 
+
+  def breed 
+    @breed
+  end 
+  
+  def breed= (breed)
+    @breed = breed 
+  end 
+end
+
+class Person 
+  def name 
+    @name 
+  end 
+  
+  def name= (name)
+    @name = name 
+  end 
+ 
+  def job 
+    @job 
+  end 
+  
+  def job= (job) 
+    @job = job 
+  end
+end
+  
